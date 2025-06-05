1181b52d039af7f22faeadd97e10b578aeef4e0e
Olga Riabenko <me@oriabenko.ru>
2025-06-05 13:01:41 +0300

    вынесение headers в переменную

--- user_classes/wt_base_scenario.py
+++ user_classes/wt_base_scenario.py
@@ -5,6 +5,11 @@ from utils.assertion import check_http_response
 from utils.non_test_methods import open_csv_field
 import random
 
+COMMON_HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate'
+}
+
 class PurchaseFlightTicket(SequentialTaskSet):
 
     test_users_csv_file_path = './test_data/user_data_test.csv'
@@ -19,20 +24,14 @@ class PurchaseFlightTicket(SequentialTaskSet):
             self.client.get(
                 '/WebTours/',
                 name='REQ_01_1_/WebTours/',
-                headers={
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate'
-                },
+                headers=COMMON_HEADERS,
                 #debug_stream=sys.stderr
             )
 
             self.client.get(
                 '/cgi-bin/welcome.pl?signOff=true',
                 name='REQ_01_2_/cgi-bin/welcome.pl?signOff=true',
-                headers={
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate'
-                },
+                headers=COMMON_HEADERS,
                 allow_redirects=False,
                 #debug_stream=sys.stderr
             )
@@ -40,10 +39,7 @@ class PurchaseFlightTicket(SequentialTaskSet):
             with self.client.get(
                 '/cgi-bin/nav.pl?in=home',
                 name='REQ_01_3_/cgi-bin/nav.pl?in=home',
-                headers={
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate'
-                },
+                headers=COMMON_HEADERS,
                 allow_redirects=False,
                 catch_response=True,
                 # debug_stream=sys.stderr
@@ -77,10 +73,7 @@ class PurchaseFlightTicket(SequentialTaskSet):
             with self.client.get(
                 '/cgi-bin/login.pl?intro=true',
                 name='REQ_02_2_/cgi-bin/login.pl?intro=true',
-                headers={
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate'
-                },
+                headers=COMMON_HEADERS,
                 # debug_stream=sys.stderr
             ) as req_02_2_response:
                 check_http_response(req_02_2_response, f"Welcome, <b>{userName}</b>")
@@ -91,19 +84,13 @@ class PurchaseFlightTicket(SequentialTaskSet):
             self.client.get(
                 '/cgi-bin/welcome.pl?signOff=1',
                 name='REQ_03_1_/cgi-bin/welcome.pl?signOff=1',
-                headers={
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate'
-                },
+                headers=COMMON_HEADERS,
             )
 
             self.client.get(
                 '/cgi-bin/nav.pl?in=home',
                 name='REQ_03_2_/cgi-bin/cgi-bin/nav.pl?in=home',
-                headers={
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Encoding': 'gzip, deflate'
-                },
+                headers=COMMON_HEADERS,
             )
 
         uc_01_getHomePage(self)
